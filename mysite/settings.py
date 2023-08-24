51b64315a276fefe6c7354715a5bc452ccbbef2f
Nikolai <nicolas.gordienko@gmail.com>
2023-08-24 22:27:12 +0200

    feat: staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['mysite-production-b04c.up.railway.app',
+ALLOWED_HOSTS = ['nikogordienko.up.railway.app',
                  '127.0.0.1']
 AUTH_USER_MODEL = 'user.User'
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
