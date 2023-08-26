35425cf32d46943422d66b1b106d6c1213f48cea
Nikolai <nicolas.gordienko@gmail.com>
2023-08-26 20:40:34 +0200

    feat: added city, country to experience

--- mysite/experience/forms.py
+++ mysite/experience/forms.py
@@ -5,4 +5,4 @@ from .models import Experience
 class ExperienceForm(forms.ModelForm):
     class Meta:
         model = Experience
-        fields = ['job_title', 'company', 'start_date', 'end_date', 'description']
+        fields = ['job_title', 'company', 'city', 'country', 'start_date', 'end_date', 'description']
--- mysite/experience/models.py
+++ mysite/experience/models.py
@@ -6,6 +6,8 @@ class Experience(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     job_title = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
     start_date = models.CharField(max_length=100)
     end_date = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
