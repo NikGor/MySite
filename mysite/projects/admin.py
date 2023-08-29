923d1afab25d13894008b35ef61d9e37c9a8f350
Nikolai <nicolas.gordienko@gmail.com>
2023-08-29 22:46:51 +0200

    feat: improved project cards

--- mysite/projects/admin.py
+++ mysite/projects/admin.py
@@ -1,6 +1,37 @@
+import os
 from django.contrib import admin
+from django.contrib.sites import requests
 from django.utils.html import format_html
 from .models import Project
+
+
+def create_screenshot(modeladmin, request, queryset):
+    screenshot_folder = 'static/screenshots'
+
+    # Создать папку screenshots, если она не существует
+    if not os.path.exists(screenshot_folder):
+        os.makedirs(screenshot_folder)
+
+    for project in queryset:
+        url_to_capture = project.url if project.url else project.github_url
+
+        if url_to_capture:
+            api_url = "https://api.apiflash.com/v1/urltoimage"
+            params = {
+                "access_key": "d1753a1756d346b690a66246fee2d66c",
+                "url": url_to_capture
+            }
+            response = requests.get(api_url, params=params)
+            if response.status_code == 200:
+                screenshot_filename = f"{project.id}_{project.name}.png"
+                screenshot_path = os.path.join(screenshot_folder, screenshot_filename)
+                with open(screenshot_path, "wb") as f:
+                    f.write(response.content)
+                project.screenshot_url = f'/screenshots/{screenshot_filename}'
+                project.save()
+
+
+create_screenshot.short_description = "Create screenshot using ApiFlash"
 
 
 @admin.register(Project)
@@ -10,6 +41,7 @@ class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     ordering = ('order',)
     list_editable = ('order', 'is_visible')
+    actions = [create_screenshot]
 
     def github_link(self, obj):
         if obj.github_url:
--- mysite/utils.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-
-def create_screenshot(url, save_path):
-    driver = webdriver.Firefox()
-    driver.get(url)
-    driver.save_screenshot(save_path)
-    driver.quit()
